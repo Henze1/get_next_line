de72725e09f3a6f037d4e7878b4d11dff0ea8a2f
Henze1 <podratchyanhenzel@gmail.com>
2025-02-12 10:37:39 +0400

    minor changes

--- get_next_line.c
+++ get_next_line.c
@@ -2,5 +2,29 @@
 
 char	*get_next_line(int fd)
 {
+	char	buff[BUFF];
+
 	return (NULL);
+}
+
+int	main(void)
+{
+	const char	*FILE;
+	int		fd;
+	char		*line;
+	
+	FILE = "get_next_line.c";
+	fd = open(FILE, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	line = get_next_line(fd);
+	while (line)
+	{
+		write(1, line, ft_strlen(line));
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,8 +1,12 @@
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
+# define BUFF 1024
 # include <unistd.h>
+# include <fcntl.h>
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *str);
 
 #endif
